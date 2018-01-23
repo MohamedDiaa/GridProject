c1b202ba6a6e1521a0d8eb85f11e217c86226afa
Diaa Alwakil <diaa.alwakil@svea.com>
2018-01-23 14:23:38 +0100

    Initial Commit

---
+++ GridProject/GridView.swift
@@ -0,0 +1,63 @@
+//
+//  GridView.swift
+//  GridProject
+//
+//  Created by Diaa Alwakil on 2018-01-23.
+//  Copyright © 2018 Diaa Alwakil. All rights reserved.
+//
+
+import UIKit
+
+struct Line{
+    let start: CGPoint
+    let end: CGPoint
+}
+
+class GridView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    let numberOfLines:CGFloat = 100
+    override func draw(_ rect: CGRect) {
+
+        //Get graphics context
+        guard let context = UIGraphicsGetCurrentContext()
+            else { return }
+        
+        context.setStrokeColor(UIColor.lightGray.cgColor)
+        context.setLineWidth(0.2)
+
+        _ = stride(from: 0, to: rect.size.width, by: rect.size.width/numberOfLines).flatMap{
+            xOffset in
+            
+            let line = Line(start: CGPoint(x: xOffset, y: 0), end: CGPoint(x: xOffset, y: rect.size.height))
+            context.draw(line: line)
+        }
+        
+        _ = stride(from: 0, to: rect.size.height, by: rect.size.height/numberOfLines).flatMap{
+            yOffset in
+            
+            let line = Line(start: CGPoint(x: 0, y: yOffset), end: CGPoint(x: rect.size.width, y: yOffset))
+            context.draw(line: line)
+        }
+        //Draw vertical lines
+        
+        //Draw horizontal lines
+        
+        //Done
+    }
+}
+
+extension CGContext{
+    
+    func draw(line:Line){
+        self.move(to: line.start)
+        self.addLine(to: line.end)
+        self.strokePath()
+    }
+}
---
+++ GridProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GridProject
+//
+//  Created by Diaa Alwakil on 2018-01-23.
+//  Copyright © 2018 Diaa Alwakil. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
