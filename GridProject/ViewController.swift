9d22ff85c93e37aa1d810b5bd95d5d3e4c9db09a
Diaa Alwakil <diaa.alwakil@svea.com>
2018-01-30 10:49:03 +0100

    Fixed Pages in PageViewController

--- GridProject/ViewController.swift
+++ GridProject/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let pageController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
+    lazy var vcs:[UIViewController] = [PointMarkerViewController(),CircleGridViewController(),GridViewController()]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+     
     }
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.pageController.view.frame = self.view.bounds
+        self.addChildViewController(pageController)
+        self.view.addSubview(pageController.view)
+        self.pageController.dataSource = self
+        self.pageController.setViewControllers([vcs[0]], direction: .forward, animated: false, completion: nil)
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -23,3 +36,22 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController:UIPageViewControllerDataSource{
+    
+    
+
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        
+        guard let index = vcs.index(of: viewController),index+1 < vcs.count
+            else { return nil }
+        print("viewControllerAfter \(index+1)")
+        return vcs[index+1]
+    }
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        
+        guard let index = vcs.index(of: viewController),index > 0
+            else { return nil }
+        print("viewControllerBefore \(index)")
+        return vcs[index - 1]
+    }
+}
